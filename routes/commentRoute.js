59a357da6451d201846464b525f121938871a45e
Vladimir <vsimke@hotmail.com>
2019-08-30 16:59:37 +0300

    Final commit with db folder

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { commentAction, getCommentById, addNewComment } from '../controllers/commentController';
 
 const router = express.Router();
-
+//
 router.get('/', commentAction);
-router.post('/:commentId', addNewComment);
+// router.post('/:commentId', addNewComment);
+router.post('/posts/:commentId/comments', addNewComment);
+
 router.get('/:commentId', getCommentById);
 
 export default router;
